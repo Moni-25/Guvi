d187f659ec1d1987c0fd65d86a07077cdc58a1ec
Moni-25 <mariyacelestina@gmail.com>
2023-12-30 14:14:02 +0530

    React Component  Class and Function Based Task Completed

---
+++ ReactComponent-Day-4/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+import FunctionBasedComponent from './Components/FunctionBased';
+
+function App() {
+  // used for ClassBased.jsx
+  //const count = [0,1,2,3,4];
+
+  return (
+    <>
+      {/* <ClassBasedComponent count={count}/> */}
+      <FunctionBasedComponent/>
+    </>
+  )
+}
+
+export default App
